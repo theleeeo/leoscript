0b70ac96c7aeab750cc365ea604eaffaf4338a94
Leonard Forsberg <leonard.forsberg@vx.se>
2025-08-12 12:44:36 +0200

    add a benchmark for comparing the interpreter and VM

---
+++ runtime/benchmark_test.go
@@ -0,0 +1,70 @@
+package runtime
+
+import (
+	"leoscript/compiler"
+	"leoscript/lexer"
+	"leoscript/parser"
+	"testing"
+)
+
+func Benchmark_Arithmetic(b *testing.B) {
+	b.Run("ast-walking interpreter", func(b *testing.B) {
+		lx := lexer.MustTokenize(`
+			1 + 2 * 3 - 4 / 2;
+		`)
+		expr, _ := parser.NewParser(lx).ParseExpr()
+		i := NewInterpreter(nil)
+
+		b.ResetTimer()
+		for b.Loop() {
+			i.evaluateExpression(expr)
+		}
+	})
+
+	b.Run("bytecode VM", func(b *testing.B) {
+		lx := lexer.MustTokenize(`
+		1 + 2 * 3 - 4 / 2;
+		`)
+		expr, _ := parser.NewParser(lx).ParseExpr()
+		exe := compiler.CompileStatement(expr)
+		vm := NewVM(exe)
+
+		b.ResetTimer()
+		for b.Loop() {
+			vm.Run()
+		}
+	})
+}
+
+func Benchmark_Fibonacci(b *testing.B) {
+	b.Run("ast-walking interpreter", func(b *testing.B) {
+
+		lx := lexer.MustTokenize(`
+		fn fib(int n) int {
+			if n <= 1 {
+				return n;
+				}
+				return fib(n - 1) + fib(n - 2);
+				}
+				`)
+		pg, err := parser.NewParser(lx).Parse()
+		if err != nil {
+			b.Fatalf("Parse error: %v", err)
+		}
+		i, err := NewInterpreter(pg).Initialize()
+		if err != nil {
+			b.Fatalf("Interpreter init error: %v", err)
+		}
+
+		b.ResetTimer()
+		for b.Loop() {
+			resp, err := i.Invoke("fib", 20)
+			if err != nil {
+				b.Fatalf("Interpreter run error: %v", err)
+			}
+			if resp.(int) != 6765 {
+				b.Fatalf("Expected 6765, got %v", resp)
+			}
+		}
+	})
+}
--- runtime/interpreter_test.go
+++ runtime/interpreter_test.go
@@ -818,36 +818,3 @@ func Test_Recursive(t *testing.T) {
 		assert.Equal(t, 55, resp.(int))
 	})
 }
-
-func Benchmark_Fibonacci(b *testing.B) {
-	b.Run("ast-walking interpreter", func(b *testing.B) {
-
-		lx := lexer.MustTokenize(`
-		fn fib(int n) int {
-			if n <= 1 {
-				return n;
-				}
-				return fib(n - 1) + fib(n - 2);
-				}
-				`)
-		pg, err := parser.NewParser(lx).Parse()
-		if err != nil {
-			b.Fatalf("Parse error: %v", err)
-		}
-		i, err := NewInterpreter(pg).Initialize()
-		if err != nil {
-			b.Fatalf("Interpreter init error: %v", err)
-		}
-
-		b.ResetTimer()
-		for b.Loop() {
-			resp, err := i.Invoke("fib", 20)
-			if err != nil {
-				b.Fatalf("Interpreter run error: %v", err)
-			}
-			if resp.(int) != 6765 {
-				b.Fatalf("Expected 6765, got %v", resp)
-			}
-		}
-	})
-}
